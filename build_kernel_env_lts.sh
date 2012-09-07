6dcc3cc453cf3dab13e5b8193eba83bab59ea447
aix27249 <i27249@gmail.com>
2012-09-07 20:43:18 +0000

    Scripts to build kernel modules for LTS kernel

---
+++ build_kernel_env_lts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KVER=${KVER:-3.0.42}
+
+#mkpkg -si -bt api:kernel -bv $KVER
+MODLIST="nvidia-kernel-lts \
+nvidia-kernel-legacy173-lts \
+nvidia-kernel-legacy96-lts \
+vhba-module-lts \
+virtualbox-kernel-lts \
+broadcom-wl-lts"
+
+
+for i in $MODLIST ; do
+	KERNEL=${KVER}lts mkpkg -bt api:$i
+done
+
